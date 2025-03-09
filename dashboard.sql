1e92641053b03fe7a68071323373169004f2f394
Darina <95154243+Erweisenn@users.noreply.github.com>
2025-03-09 13:30:54 +0300

    Add files via upload

--- dashboard.sql
+++ dashboard.sql
@@ -1,51 +1,73 @@
-with sessions_with_paid_mark as (
-    select
-        *,
-        case
-            when
-                medium in (
-                    'cpc',
-                    'cpm',
-                    'cpa',
-                    'youtube',
-                    'cpp',
-                    'tg',
-                    'social'
-                )
-                then 1
-            else 0
-        end as is_paid
-    from sessions
+WITH costs AS (
+    SELECT 
+        utm_source,
+        SUM(daily_spent) AS total_cost
+    FROM 
+        (
+            SELECT 
+                utm_source,
+                daily_spent
+            FROM 
+                vk_ads
+            UNION ALL
+            SELECT 
+                utm_source,
+                daily_spent
+            FROM 
+                ya_ads
+        ) AS combined_ads
+    GROUP BY 
+        utm_source
 ),
-
-visitors_with_leads as (
-    select
-        s.visitor_id,
-        s.visit_date,
-        s.source,
-        s.medium,
-        s.campaign,
-        l.created_at,
-        l.amount,
-        l.closing_reason,
-        l.status_id,
-        row_number() over (
-            partition by s.visitor_id
-            order by s.is_paid desc, s.visit_date desc
-        ) as rn
-    from sessions_with_paid_mark as s
-    left join leads as l
-        on
-            l.visitor_id = s.visitor_id
-            and l.created_at >= s.visit_date
+visitors AS (
+    SELECT 
+        source AS utm_source,
+        COUNT(DISTINCT visitor_id) AS visitors_count
+    FROM 
+        sessions
+    GROUP BY 
+        source
+),
+leads AS (
+    SELECT 
+        s.source AS utm_source,
+        COUNT(DISTINCT l.lead_id) AS leads_count
+    FROM 
+        sessions s
+    LEFT JOIN 
+        leads l ON s.visitor_id = l.visitor_id
+    GROUP BY 
+        s.source
+),
+purchases AS (
+    SELECT 
+        s.source AS utm_source,
+        COUNT(DISTINCT l.lead_id) AS purchases_count,
+        SUM(l.amount) AS total_revenue
+    FROM
+        sessions s
+    JOIN 
+        leads l 
+        ON s.visitor_id = l.visitor_id
+    GROUP BY 
+        s.source
 )
-
-select
-    source,
-    medium,
-    percentile_disc(0.90) within group (
-        order by date_part('day', created_at - visit_date)
-    ) as days_to_lead
-from visitors_with_leads
-where rn = 1
-group by 1, 2
+SELECT 
+    v.utm_source,
+    v.visitors_count,
+    l.leads_count,
+    p.purchases_count,
+    c.total_cost,
+    p.total_revenue,
+    (c.total_cost / v.visitors_count) AS cpu,
+    (c.total_cost / l.leads_count) AS cpl,
+    (c.total_cost / p.purchases_count) AS cppu,
+    ((p.total_revenue - c.total_cost) / c.total_cost) * 100 AS roi
+FROM 
+    visitors v
+JOIN 
+    leads l ON v.utm_source = l.utm_source
+JOIN 
+    purchases p ON v.utm_source = p.utm_source
+JOIN 
+    costs c ON v.utm_source = c.utm_source;
